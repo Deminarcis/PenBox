c0d8ac3342ea6594680433dfa5f9329410ffda9c
Deminarcis <jake.maher.10@gmail.com>
2018-07-16 01:12:22 +1000

    Added PS script to bring some of these great tools to windows without WSL

---
+++ pentools-windows.ps1
@@ -0,0 +1,10 @@
+#!/usr/bin/pwsh-preview
+## TODO: - Figure out remote install
+#        - can i get it signed? so AMSI deosnt have a hissy fit.
+#        - follow best practice
+#        - detect user
+#        - install in Appdata (home directory)
+#        - Leverage wget? do we need curl?
+#        - Minimal bloat -- Windows takes 40GB itself, use as much inbuilt as you can, dont bloat the sysytem
+#        - add defender exception for directory?
+#        - How do I make a variable?
