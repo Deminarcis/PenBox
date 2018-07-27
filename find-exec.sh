84ebf84379a4b58e9a835135e4df3cba21d1d5ba
Jacob Maher <jake.maher.10@gmail.com>
2018-07-28 00:34:34 +1000

    formatting

--- find-exec.sh
+++ find-exec.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 sourceFolder="$PWD"
-read -p "Where did you install pentools?" runFolder
+read -p "Where did you install pentools?  " runFolder
 echo "Finding executable files  in $runFolder and printing them to files.executable in $sourceFolder"
 find $runFolder -type f -executable > $sourceFolder/files.executable
