d3903f8ea3b53c7eada4dd8e5d9f341bbcabdcd3
Deminarcis <jake.maher.10@gmail.com>
2018-11-08 23:11:20 +1100

    bump

--- pentools-windows.ps1
+++ pentools-windows.ps1
@@ -1,11 +1,11 @@
 #!/usr/bin/pwsh-preview
 ## TODO: - Figure out remote install
-#        - can i get it signed? so AMSI deosnt have a hissy fit.
+#        - can i get it signed? so AMSI doesn't have a hissy fit.
 #        - follow best practice
 #        - detect user
 #        - install in Appdata (home directory)
 #        - Leverage wget? do we need curl?
-#        - Minimal bloat -- Windows takes 40GB itself, use as much inbuilt as you can, dont bloat the sysytem
+#        - Minimal bloat -- Windows takes 40GB itself, use as much inbuilt as you can, dont bloat the system
 #        - add defender exception for directory?
 #        - How do I make a variable?
 #        - How do functions work?
