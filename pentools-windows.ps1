d7d383c8ba216b3a08181d9eab5e2642ad72a8f7
Deminarcis <jake.maher.10@gmail.com>
2019-01-13 13:33:15 +1100

    started building windows script (it's not ready)

--- pentools-windows.ps1
+++ pentools-windows.ps1
@@ -1,12 +1,9 @@
-#!/usr/bin/pwsh-preview
-## TODO: - Figure out remote install
-#        - can i get it signed? so AMSI doesn't have a hissy fit.
-#        - follow best practice
-#        - detect user
-#        - install in Appdata (home directory)
-#        - Leverage wget? do we need curl?
-#        - Minimal bloat -- Windows takes 40GB itself, use as much inbuilt as you can, dont bloat the system
-#        - add defender exception for directory?
-#        - How do I make a variable?
-#        - How do functions work?
-#        - utilize GIT without a GUI in Windows
+### Variables
+$dir='%APPDATA%\pentools'
+
+### Welcome
+echo "Welcome to Pentools, this may work, it may not. This exists to help me learn powershell"
+echo "Making a folder in appdata to put all this stuff"
+mkdir \$dir
+echo "Installing choco to make this easier"
+Start-Process powershell -Verb runAs Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
