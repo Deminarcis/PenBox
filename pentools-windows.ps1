85aae19672a4495be343e7edc1164e475fcd1993
Deminarcis <jake.maher.10@gmail.com>
2019-01-15 23:44:39 +1100

    added stuff

--- pentools-windows.ps1
+++ pentools-windows.ps1
@@ -6,4 +6,8 @@ echo "Welcome to Pentools, this may work, it may not. This exists to help me lea
 echo "Making a folder in appdata to put all this stuff"
 mkdir \$dir
 echo "Installing choco to make this easier"
+###This line doesn't work but moving on for now
 Start-Process powershell -Verb runAs Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+### Calling general dependencies to get this to work
+choco install python ruby pip rake git
+### make functions like the other platforms
