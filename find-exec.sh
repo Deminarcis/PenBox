ebe22c09013b972fc4236efe3776a96ec45a549c
Deminarcis <jake.maher.10@gmail.com>
2019-01-24 11:13:56 +1100

    updated find script to narrow search terms

--- find-exec.sh
+++ find-exec.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-
+#Variables
 sourceFolder="$PWD"
+#Run Chunk
 read -p "Where did you install pentools?  " runFolder
 echo "Finding executable files  in $runFolder and printing them to files.executable in $sourceFolder"
-find $runFolder -type f -executable > $sourceFolder/files.executable
+find $runFolder -type f -executable ! -name '*.c' ! -name '*.h' ! -name '*.cs' ! -name '*.cpp' ! -name '*.sample' ! -name '[Mm]akefile' ! -name '*.gcc'  > $sourceFolder/files.executable
