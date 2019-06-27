beeb62d6c0da6d176786f3187036c59bbe765f88
Deminarcis <jake.maher.10@gmail.com>
2019-06-28 00:00:19 +1000

    bacon

--- find-exec.py
+++ find-exec.py
@@ -21,7 +21,12 @@ os.system("mkdir -p /home/'$USER'/.local/bin")
 #Run Chunk
 os.system("read -p 'Where did you install pentools?  ' runFolder")
 print("Finding executable files  in $runFolder")
-os.system("find $runFolder -type f -executable -name '*.py' -o -name '*.rb' -o -name '*.sh' -exec 'ln -s '{}' '/home/$USER/.local/bin''")
+#os.system("find $runFolder -type f -executable -name '*.py' -o -name '*.rb' -o -name '*.sh' -exec 'ln -s '{}' '/home/$USER/.local/bin''")
+fnames = ([file for root, dirs, files in os.walk(dir)
+    for file in files
+    if file.endswith('.sh') or file.endswith('.py') or file.endswith('.rb') #or file.endswith('.png') or file.endswith('.pdf')
+    ])
+for fname in fnames: os.system("ln -s %s /home/$USER/.local/bin" % fnames)
 if os.access("~/.bashrc", os.R_OK):
     update_bashrc()
 if os.access("~/.zshrc", os.R_OK):
