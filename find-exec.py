37dee196668bbe47349db6b41daf04821fba8014
jmaher <jmaher@localhost.localdomain>
2019-06-29 13:21:20 +1000

    kinda works now

--- find-exec.py
+++ find-exec.py
@@ -3,30 +3,34 @@
 import os
 import platform
 
+# Vars
+user = input("What is your username? ")
+os.system("mkdir -p /home/%s/.local/bin" % (user))
+os.system("ln -s /home/%s/.local/bin /home/%s/bin" % (user, user))
+
 ### Functions
 
 def update_bashrc():
     open("~/.bashrc", "a")
     with open("~/.bashrc", "a") as bashrc:
-        bashrc.write("PATH=$PATH:/home/$USER/bin")
+        bashrc.write("PATH=$PATH:/home/%s/bin" % (user))
     bashrc.close()
 
 def update_zshrc():
     open("~/.zshrc", "a")
     with open("~/.zshrc", "a") as zshrc:
-        zshrc.write("PATH=$PATH:/home/$USER/bin")
+        zshrc.write("PATH=$PATH:/home/%s/bin" % (user))
     zshrc.close()
 
-os.system("mkdir -p /home/'$USER'/.local/bin")
 #Run Chunk
 dir = '/opt/haxxx'
-print("Finding executable files  in $runFolder")
-#os.system("find $runFolder -type f -executable -name '*.py' -o -name '*.rb' -o -name '*.sh' -exec 'ln -s '{}' '/home/$USER/.local/bin''")
+print("Finding executable files in /opt/haxxx")
 fnames = ([file for root, dirs, files in os.walk(dir)
     for file in files
     if file.endswith('.sh') or file.endswith('.py') or file.endswith('.rb') #or file.endswith('.png') or file.endswith('.pdf')
     ])
-for fname in fnames: os.system("ln -s %s /home/$USER/.local/bin" % fname)
+for fname in fnames: 
+    os.system("ln -s %s /home/%s/.local/bin" % (fname, user))
 if os.access("~/.bashrc", os.R_OK):
     update_bashrc()
 if os.access("~/.zshrc", os.R_OK):
