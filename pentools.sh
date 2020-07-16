952bcf37781ced9dad3f603ef82facb6db438e34
Deminarcis <jake.maher.10@gmail.com>
2020-07-16 22:18:13 +1000

    poo

--- pentools.sh
+++ pentools.sh
@@ -5,8 +5,8 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 # Bookmarking where the script was run from
-export runFolder = "$PWD"
-export myDirectory = "/opt/haxxx"
+runFolder="$PWD"
+myDirectory="/opt/haxxx"
 
 #Install Groups (lists for OS at the bottom)
 
