374f1c27faa936fc505633991d55d7b3b6551d5e
Deminarcis.rb <jake.maher.10@gmail.com>
2020-12-05 18:46:17 +1100

    created deploy and helper for podman

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
-#!/usr/bin/env bash
+mkdir -p ~/.local/bin
+cp start-kali-pod ~/.local/bin
+cp stop-kali-pod ~/.local/bin
+podman run --name kali-pod --net=host --privileged -e DISPLAY=:0 -it -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/podman-storage/Kali:/Shared kalilinux/kali-rolling /bin/bash
