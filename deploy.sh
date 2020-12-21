f64a9cebc02463ee1e63dcf9530c9c01bf907f4f
Deminarcis <jake.maher.10@gmail.com>
2020-12-21 23:14:58 +1100

    - Updated scripts to give feedback on the process. - Changed container name to kali from kali-pod

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
+echo '### Deploying helper script'
 mkdir -p ~/.local/bin
-cp start-kali-pod ~/.local/bin
-cp stop-kali-pod ~/.local/bin
-podman run --name kali-pod --net=host --privileged -e DISPLAY=:0 -it -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/podman-storage/Kali:/Shared kalilinux/kali-rolling /bin/bash
+cp start-kali ~/.local/bin
+cp stop-kali ~/.local/bin
+echo '### Pulling Pod from the internet and installing'
+podman run --name Kali --net=host --privileged -e DISPLAY=:0 -it -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/podman-storage/Kali:/Shared kalilinux/kali-rolling /bin/bash
+echo -e 'Setup done, start and stop scripts are set up for this user and kept in `~/.local/bin` you may wish to add the directory to your path (export PATH=$PATH:$HOME/.local/bin ) or move them somewhere else.'
