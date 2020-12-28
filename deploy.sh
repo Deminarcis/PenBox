f204b42a8115caae7b4082f50d20ccd2a27aefd2
Deminarcis.rb <jake.maher.10@gmail.com>
2020-12-28 23:13:19 +1100

    removed port fwd

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ mkdir -p ~/.local/bin
 cp start-kali ~/.local/bin
 cp stop-kali ~/.local/bin
 echo '### Pulling Pod from the internet and installing'
-podman run --name Kali --net=host --privileged -e DISPLAY=:0 -it -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/podman-storage/Kali:/Shared kalilinux/kali-rolling -p 2222:22 /bin/bash
+podman run --name Kali --net=host --privileged -e DISPLAY=:0 -it -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/podman-storage/Kali:/Shared kalilinux/kali-rolling /bin/bash
 echo -e 'Setup done, start and stop scripts are set up for this user and kept in `~/.local/bin` you may wish to add the directory to your path (export PATH=$PATH:$HOME/.local/bin ) or move them somewhere else.'
