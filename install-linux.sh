90207426eacf9d41dd3c37bca0093ea7c247832b
Deminarcis.rb <jake.maher.10@gmail.com>
2021-11-07 19:46:01 +1100

    Updated if statements, moved storage mount to a hidden directory

--- install-linux.sh
+++ install-linux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-#Arch based distro
-if  [ -f /usr/bin/pacman ]; then
+#Arch/Manjaro
+if  [[ -f /usr/bin/pacman ]]; then
     sudo pacman -S podman --needed
     sudo touch /etc/subuid
     sudo touch /etc/subgid
@@ -8,24 +8,42 @@ if  [ -f /usr/bin/pacman ]; then
     echo 'see "https://wiki.archlinux.org/index.php/Cgroups#Switching_to_cgroups_v2" for help setting up cgroups2'
 fi
 
-if [ -f /usr/bin/apt ]; then
+#Debian/Ubuntu
+if [[ -f /usr/bin/apt ]]; then
     echo "Installing podman from repos. Further setup may be needed, please check your distro's documentation for podman or Cgroups v2"
     sudo apt install -y podman
 fi
 
-if [ -f /usr/bin/zypper ]; then
+#OpenSUSE
+if [[ -f /usr/bin/zypper ]]; then
     echo "Installing podman from repos. Further setup may be needed, please check your distro's documentation for podman or Cgroups v2"
     sudo zypper in -y podman
 fi
 
-if [ -f /usr/bin/dnf ]; then
-    if [ -f /usr/bin/podman ]; then
+#Fedora/RHEL
+if [[ -f /usr/bin/dnf ]]; then
+    if [[ -f /usr/bin/podman ]]; then
         echo "Podman is already configured in RHEL systems as needed, moving on."
     else
         sudo dnf install -y podman
     fi
 fi
-echo '### Pulling Pod from the internet and installing'
-podman run -dt --name Kali --net=host --privileged -e DISPLAY=:0 -it -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/podman-storage/Kali:/Shared kalilinux/kali-rolling /bin/bash
+
+echo "### Checking the environment is suitable"
+if [[ ! -d ~/.config/systemd/user/ ]]; then
+    mkdir -p ~/.config/systemd/user/
+fi
+if [[ ! -d ~/.podman-storage/ ]]; then
+    mkdir -p ~/.podman-storage/
+fi
+
+echo "### Pulling Pod from the internet and installing"
+echo "### If you don't know which one to pick, pick the docker.io"
+echo "### The following permissions wil be set on the container
+
+Networking: Copy from host user
+Storage: A shared mountpoint will be used at ~/.podman-storage
+"
+podman run -dt --name Kali --net=host --privileged -e DISPLAY=:0 -it -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/.podman-storage/Kali:/Shared kalilinux/kali-rolling /bin/bash
 echo -e 'Setting up systemd files'
 podman generate systemd --name Kali > ~/.config/systemd/user/container-kali.service
