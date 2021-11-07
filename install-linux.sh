32cbc4b0e0eb3b30bf94fadd423ed7bff86ac788
Deminarcis.rb <jake.maher.10@gmail.com>
2021-11-07 19:58:44 +1100

    reverted if statement brackets

--- install-linux.sh
+++ install-linux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Arch/Manjaro
-if  [[ -f /usr/bin/pacman ]]; then
+if  [ -f /usr/bin/pacman ]; then
     sudo pacman -S podman --needed
     sudo touch /etc/subuid
     sudo touch /etc/subgid
@@ -9,20 +9,20 @@ if  [[ -f /usr/bin/pacman ]]; then
 fi
 
 #Debian/Ubuntu
-if [[ -f /usr/bin/apt ]]; then
+if [ -f /usr/bin/apt ]; then
     echo "Installing podman from repos. Further setup may be needed, please check your distro's documentation for podman or Cgroups v2"
     sudo apt install -y podman
 fi
 
 #OpenSUSE
-if [[ -f /usr/bin/zypper ]]; then
+if [ -f /usr/bin/zypper ]; then
     echo "Installing podman from repos. Further setup may be needed, please check your distro's documentation for podman or Cgroups v2"
     sudo zypper in -y podman
 fi
 
 #Fedora/RHEL
-if [[ -f /usr/bin/dnf ]]; then
-    if [[ -f /usr/bin/podman ]]; then
+if [ -f /usr/bin/dnf ]; then
+    if [ -f /usr/bin/podman ]; then
         echo "Podman is already configured in RHEL systems as needed, moving on."
     else
         sudo dnf install -y podman
@@ -30,10 +30,10 @@ if [[ -f /usr/bin/dnf ]]; then
 fi
 
 echo "### Checking the environment is suitable"
-if [[ ! -d ~/.config/systemd/user/ ]]; then
+if [ ! -d ~/.config/systemd/user/ ]; then
     mkdir -p ~/.config/systemd/user/
 fi
-if [[ ! -d ~/.podman-storage/ ]]; then
+if [ ! -d ~/.podman-storage/ ]; then
     mkdir -p ~/.podman-storage/
 fi
 
