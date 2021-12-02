e159bacb3327196c6db0c1e1a5190f23c6bfbfaa
Deminarcis <jake.maher.10@gmail.com>
2021-12-03 00:12:38 +1100

    added daemon-reload in case of re-install via installer

--- install-linux.sh
+++ install-linux.sh
@@ -47,3 +47,4 @@ Storage: A shared mountpoint will be used at ~/.podman-storage
 podman run -dt --name Kali --net=host --privileged -e DISPLAY=:0 -it -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/.podman-storage/Kali:/Shared kalilinux/kali-rolling /bin/bash
 echo -e 'Setting up systemd files'
 podman generate systemd --name Kali > ~/.config/systemd/user/container-kali.service
+systemctl --user daemon-reload
